5597c60bb5508fd339cb35238fcf6c21c62e0fef
elodie-boweren <elodie.boweren@laplateforme.io>
2025-11-12 11:59:33 +0100

    fix: font problem on windows

---
+++ build/CMakeFiles/CyberpunkCannonShooter.dir/DependInfo.cmake
@@ -0,0 +1,39 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/elodi/OneDrive/Desktop/Projets_plateforme/LOGICIEL/B2/C++/Casse-Brique/CasseBrick/src/core/FontManager.cpp" "CMakeFiles/CyberpunkCannonShooter.dir/src/core/FontManager.cpp.obj" "gcc" "CMakeFiles/CyberpunkCannonShooter.dir/src/core/FontManager.cpp.obj.d"
+  "C:/Users/elodi/OneDrive/Desktop/Projets_plateforme/LOGICIEL/B2/C++/Casse-Brique/CasseBrick/src/core/Game.cpp" "CMakeFiles/CyberpunkCannonShooter.dir/src/core/Game.cpp.obj" "gcc" "CMakeFiles/CyberpunkCannonShooter.dir/src/core/Game.cpp.obj.d"
+  "C:/Users/elodi/OneDrive/Desktop/Projets_plateforme/LOGICIEL/B2/C++/Casse-Brique/CasseBrick/src/core/GameState.cpp" "CMakeFiles/CyberpunkCannonShooter.dir/src/core/GameState.cpp.obj" "gcc" "CMakeFiles/CyberpunkCannonShooter.dir/src/core/GameState.cpp.obj.d"
+  "C:/Users/elodi/OneDrive/Desktop/Projets_plateforme/LOGICIEL/B2/C++/Casse-Brique/CasseBrick/src/core/states/GameOverState.cpp" "CMakeFiles/CyberpunkCannonShooter.dir/src/core/states/GameOverState.cpp.obj" "gcc" "CMakeFiles/CyberpunkCannonShooter.dir/src/core/states/GameOverState.cpp.obj.d"
+  "C:/Users/elodi/OneDrive/Desktop/Projets_plateforme/LOGICIEL/B2/C++/Casse-Brique/CasseBrick/src/core/states/MenuState.cpp" "CMakeFiles/CyberpunkCannonShooter.dir/src/core/states/MenuState.cpp.obj" "gcc" "CMakeFiles/CyberpunkCannonShooter.dir/src/core/states/MenuState.cpp.obj.d"
+  "C:/Users/elodi/OneDrive/Desktop/Projets_plateforme/LOGICIEL/B2/C++/Casse-Brique/CasseBrick/src/core/states/PausedState.cpp" "CMakeFiles/CyberpunkCannonShooter.dir/src/core/states/PausedState.cpp.obj" "gcc" "CMakeFiles/CyberpunkCannonShooter.dir/src/core/states/PausedState.cpp.obj.d"
+  "C:/Users/elodi/OneDrive/Desktop/Projets_plateforme/LOGICIEL/B2/C++/Casse-Brique/CasseBrick/src/core/states/PlayingState.cpp" "CMakeFiles/CyberpunkCannonShooter.dir/src/core/states/PlayingState.cpp.obj" "gcc" "CMakeFiles/CyberpunkCannonShooter.dir/src/core/states/PlayingState.cpp.obj.d"
+  "C:/Users/elodi/OneDrive/Desktop/Projets_plateforme/LOGICIEL/B2/C++/Casse-Brique/CasseBrick/src/core/states/SettingsState.cpp" "CMakeFiles/CyberpunkCannonShooter.dir/src/core/states/SettingsState.cpp.obj" "gcc" "CMakeFiles/CyberpunkCannonShooter.dir/src/core/states/SettingsState.cpp.obj.d"
+  "C:/Users/elodi/OneDrive/Desktop/Projets_plateforme/LOGICIEL/B2/C++/Casse-Brique/CasseBrick/src/entities/Block.cpp" "CMakeFiles/CyberpunkCannonShooter.dir/src/entities/Block.cpp.obj" "gcc" "CMakeFiles/CyberpunkCannonShooter.dir/src/entities/Block.cpp.obj.d"
+  "C:/Users/elodi/OneDrive/Desktop/Projets_plateforme/LOGICIEL/B2/C++/Casse-Brique/CasseBrick/src/entities/Brick.cpp" "CMakeFiles/CyberpunkCannonShooter.dir/src/entities/Brick.cpp.obj" "gcc" "CMakeFiles/CyberpunkCannonShooter.dir/src/entities/Brick.cpp.obj.d"
+  "C:/Users/elodi/OneDrive/Desktop/Projets_plateforme/LOGICIEL/B2/C++/Casse-Brique/CasseBrick/src/entities/Cannon.cpp" "CMakeFiles/CyberpunkCannonShooter.dir/src/entities/Cannon.cpp.obj" "gcc" "CMakeFiles/CyberpunkCannonShooter.dir/src/entities/Cannon.cpp.obj.d"
+  "C:/Users/elodi/OneDrive/Desktop/Projets_plateforme/LOGICIEL/B2/C++/Casse-Brique/CasseBrick/src/entities/Projectile.cpp" "CMakeFiles/CyberpunkCannonShooter.dir/src/entities/Projectile.cpp.obj" "gcc" "CMakeFiles/CyberpunkCannonShooter.dir/src/entities/Projectile.cpp.obj.d"
+  "C:/Users/elodi/OneDrive/Desktop/Projets_plateforme/LOGICIEL/B2/C++/Casse-Brique/CasseBrick/src/main.cpp" "CMakeFiles/CyberpunkCannonShooter.dir/src/main.cpp.obj" "gcc" "CMakeFiles/CyberpunkCannonShooter.dir/src/main.cpp.obj.d"
+  "C:/Users/elodi/OneDrive/Desktop/Projets_plateforme/LOGICIEL/B2/C++/Casse-Brique/CasseBrick/src/managers/BlockManager.cpp" "CMakeFiles/CyberpunkCannonShooter.dir/src/managers/BlockManager.cpp.obj" "gcc" "CMakeFiles/CyberpunkCannonShooter.dir/src/managers/BlockManager.cpp.obj.d"
+  "C:/Users/elodi/OneDrive/Desktop/Projets_plateforme/LOGICIEL/B2/C++/Casse-Brique/CasseBrick/src/ui/AnimatedText.cpp" "CMakeFiles/CyberpunkCannonShooter.dir/src/ui/AnimatedText.cpp.obj" "gcc" "CMakeFiles/CyberpunkCannonShooter.dir/src/ui/AnimatedText.cpp.obj.d"
+  "C:/Users/elodi/OneDrive/Desktop/Projets_plateforme/LOGICIEL/B2/C++/Casse-Brique/CasseBrick/src/ui/Button.cpp" "CMakeFiles/CyberpunkCannonShooter.dir/src/ui/Button.cpp.obj" "gcc" "CMakeFiles/CyberpunkCannonShooter.dir/src/ui/Button.cpp.obj.d"
+  "C:/Users/elodi/OneDrive/Desktop/Projets_plateforme/LOGICIEL/B2/C++/Casse-Brique/CasseBrick/src/ui/Starfield.cpp" "CMakeFiles/CyberpunkCannonShooter.dir/src/ui/Starfield.cpp.obj" "gcc" "CMakeFiles/CyberpunkCannonShooter.dir/src/ui/Starfield.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
