5d336382b73b0f99a5b2c10b32a51d6068ebc1cd
Louis <louis.cordier@laplateforme.io>
2025-11-14 11:31:55 +0100

    Feat: Specific modifications for my own version (CMakeLists)

--- build/CTestTestfile.cmake
+++ build/CTestTestfile.cmake
@@ -1,6 +1,6 @@
 # CMake generated Testfile for 
-# Source directory: C:/Users/elodi/OneDrive/Desktop/Projets_plateforme/LOGICIEL/B2/C++/Casse-Brique/CasseBrick
-# Build directory: C:/Users/elodi/OneDrive/Desktop/Projets_plateforme/LOGICIEL/B2/C++/Casse-Brique/CasseBrick/build
+# Source directory: C:/Users/louis/Documents/Bachelor IT/B2/Projets/CasseBrick
+# Build directory: C:/Users/louis/Documents/Bachelor IT/B2/Projets/CasseBrick/build
 # 
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/elodi/OneDrive/Desktop/Projets_plateforme/LOGICIEL/B2/C++/Casse-Brique/CasseBrick
+# Install script for directory: C:/Users/louis/Documents/Bachelor IT/B2/Projets/CasseBrick
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -34,13 +34,13 @@ endif()
 
 # Set path to fallback-tool for dependency-resolution.
 if(NOT DEFINED CMAKE_OBJDUMP)
-  set(CMAKE_OBJDUMP "C:/msys64/ucrt64/bin/objdump.exe")
+  set(CMAKE_OBJDUMP "C:/msys64/mingw64/bin/objdump.exe")
 endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
 if(CMAKE_INSTALL_LOCAL_ONLY)
-  file(WRITE "C:/Users/elodi/OneDrive/Desktop/Projets_plateforme/LOGICIEL/B2/C++/Casse-Brique/CasseBrick/build/install_local_manifest.txt"
+  file(WRITE "C:/Users/louis/Documents/Bachelor IT/B2/Projets/CasseBrick/build/install_local_manifest.txt"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
 if(CMAKE_INSTALL_COMPONENT)
@@ -56,6 +56,6 @@ else()
 endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
-  file(WRITE "C:/Users/elodi/OneDrive/Desktop/Projets_plateforme/LOGICIEL/B2/C++/Casse-Brique/CasseBrick/build/${CMAKE_INSTALL_MANIFEST}"
+  file(WRITE "C:/Users/louis/Documents/Bachelor IT/B2/Projets/CasseBrick/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
