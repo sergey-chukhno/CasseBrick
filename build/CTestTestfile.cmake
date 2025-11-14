2deb75d6c6c7a1ab3ba69c5ff8b53fe464038b20
elodie-boweren <elodie.boweren@laplateforme.io>
2025-11-14 09:42:15 +0100

    fix: trying to fix gitignore problem

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/elodi/OneDrive/Desktop/Projets_plateforme/LOGICIEL/B2/C++/Casse-Brique/CasseBrick
-# Build directory: C:/Users/elodi/OneDrive/Desktop/Projets_plateforme/LOGICIEL/B2/C++/Casse-Brique/CasseBrick/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
